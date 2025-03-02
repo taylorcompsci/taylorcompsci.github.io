ddfbd10d877a3ee5449fdb122b7bf399d47c03ba
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-03-02 18:35:05 +0000

    revamped mapping system for projects.js

---
+++ assets/JSON/projects.js
@@ -0,0 +1,74 @@
+
+export default function data() {
+    return [
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        },
+        {
+            "img" : "assets/officers/cutie.png",
+            "link" : "https://github.com/taylorcompsci/taylorcompsci.github.io"
+        }
+    ];
+}
+
--- scripts/projectPage.js
+++ scripts/projectPage.js
@@ -1,19 +1,17 @@
-
+import data from "../assets/JSON/projects.js";
 
 const def =     {
     img : "assets/officers/cutie.png",
     link : "https://github.com/taylorcompsci/taylorcompsci.github.io",
 }
 
-
-
-const projects = [
-    def,def,def,def,def,def,def,def,    def,def,def,def,def,def,def,def,    def,def,def,def,def,def,def,def,    def,def,def,def,def,def,def,def,    def,def,def,def,def,def,def,def,
-];
+//seperating the script from the dataset refer to assets/JSON/projects.json
+let projects = data();
 
 const side = Math.ceil(Math.pow(projects.length, 0.5));
 
 const reload = ( link ) => {
+    console.log(link)
     window.open(link, "_blank").focus();
     window.location.reload();
 };
@@ -22,20 +20,25 @@ addEventListener("load", ()=>{
     const grid = document.querySelector(".grid");
     for(let row = 0; row < side; row++){
         
-        let rowDiv = "<div class='gridRow'>";
+        let rowDiv = document.createElement("div");
+        rowDiv.classList.add("gridRow");
         
         for(let col = 0; col < side; col++){
             let realIDX = (side*row)+col;
             if(realIDX >= projects.length)
                 break;
             let project = projects[realIDX];
-            console.log(project)
-            rowDiv += `
-        <div class="gridPiece" onclick="reload('${project.link}')">
-                <img src="${project.img}"/>
-        </div>
-        `
+            
+            const gridPiece = document.createElement("div");
+            gridPiece.classList.add("gridPiece");
+            
+            gridPiece.innerHTML = `<img src=${project.img}>`;
+
+            gridPiece.onclick = () => {reload(project.link)};
+
+            rowDiv.appendChild(gridPiece)
+
         }
-        grid.innerHTML+=rowDiv+"</div>";  
+        grid.appendChild(rowDiv);  
     }
 });
