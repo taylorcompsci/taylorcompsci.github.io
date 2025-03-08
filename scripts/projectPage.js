706b303a248f101c2e54c40e666fc250960c3fc2
Jaiden Khosla <74748528+JaidenKhosla@users.noreply.github.com>
2025-03-08 04:31:23 +0000

    backEndDavid

--- scripts/projectPage.js
+++ scripts/projectPage.js
@@ -6,26 +6,55 @@ const def =     {
 }
 
 //seperating the script from the dataset refer to assets/JSON/projects.json
-let projects = data();
 
-const side = Math.ceil(Math.pow(projects.length, 0.5));
 
 async function getProjectData(){
-    await fetch("https://5xu2xfigc4.execute-api.us-east-1.amazonaws.com/deployment", {
-        method: "GET",
-        body : {"Action":"Access"},
-        headers:{
-            "Content-Type": "application/json"
-        }
+    try{
+    const projectData = await fetch("https://5xu2xfigc4.execute-api.us-east-1.amazonaws.com/deployment", {
+        "method": "POST",
+        body: JSON.stringify({"Action":"Access"})
         
     }).then(response => {
         if(!response.ok)
-            console.error("Error occured!");
+            console.log("Error occured!");
 
-        return response.data();
+        return response.json();
     }).then(data => {
-        console.log(data.JSON);
+
+        const projects =  JSON.parse(data).body;
+        const side =  Math.ceil(Math.pow(projects.length, 0.5));
+        console.log(projects);
+        const grid = document.querySelector(".grid");
+        for(let row = 0; row < side; row++){
+            
+            let rowDiv = document.createElement("div");
+            rowDiv.classList.add("gridRow");
+            
+            for(let col = 0; col < side; col++){
+                let realIDX = (side*row)+col;
+                if(realIDX >= projects.length)
+                    break;
+                let project = projects[realIDX];
+                
+                const gridPiece = document.createElement("div");
+                gridPiece.classList.add("gridPiece");
+                
+                gridPiece.innerHTML = `<img src=${project.img}>`;
+    
+                gridPiece.onclick = () => {reload(project.link)};
+    
+                rowDiv.appendChild(gridPiece)
+    
+            }
+            grid.appendChild(rowDiv);  
+        }
+
     });
+
+    }
+    catch(error){
+        console.log(error);
+    }
 }
 
 
@@ -36,30 +65,6 @@ const reload = ( link ) => {
     window.location.reload();
 };
 
-addEventListener("load", ()=>{
+addEventListener("load", async function(){
     getProjectData();
-    const grid = document.querySelector(".grid");
-    for(let row = 0; row < side; row++){
-        
-        let rowDiv = document.createElement("div");
-        rowDiv.classList.add("gridRow");
-        
-        for(let col = 0; col < side; col++){
-            let realIDX = (side*row)+col;
-            if(realIDX >= projects.length)
-                break;
-            let project = projects[realIDX];
-            
-            const gridPiece = document.createElement("div");
-            gridPiece.classList.add("gridPiece");
-            
-            gridPiece.innerHTML = `<img src=${project.img}>`;
-
-            gridPiece.onclick = () => {reload(project.link)};
-
-            rowDiv.appendChild(gridPiece)
-
-        }
-        grid.appendChild(rowDiv);  
-    }
 });
